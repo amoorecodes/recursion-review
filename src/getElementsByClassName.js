c04fc4b62f4fe7d6fc3c360ce305fc7ff22222f0
unfaceit <ryabtsev.art@gmail.com>
2018-04-23 12:39:05 -0700

    first iteration of function pre recursion

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -7,4 +7,24 @@
 var getElementsByClassName = function(className
 ) {
   // your code here
+  /*
+  start at the top node go down through the tree
+  check every node for the class
+  check every node for children w/ class
+  use recursive pattern until reaches base case
+  base case === no more children
+  */
+
+  var parent = document.body;
+  var results = [];
+  if(parent.hasChildNodes()) {
+    var allNodes = document.body.childNodes;
+    for(var i = 0; i < allNodes.length; i++) {
+      if(allNodes[i].classList.hasOwnProperty(className)) {
+        results.push(allNodes[i]);
+      }
+      //if(allNodes[i].hasChildNodes())
+    }
+  }
+
 };
